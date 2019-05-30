c214326158e68aafd58e98feb6916d022ba81b7c
Aleksejs Fomins <fomins.aleksejs@gmail.com>
2019-05-30 23:49:55 +0200

    AR + behaviour

--- src/lib/autoregression_lib.py
+++ src/lib/autoregression_lib.py
@@ -10,15 +10,20 @@ x(t) = sum_i A(i)x(t-i) + B u(t)
 '''
 
 # Construct stacked past measurements
-def AR_STACK_LAG(data, p):
+def AR_STACK_LAG(data, p, Inp = None):
     nCh, nTr, nT = data.shape
     
     Y = data[:,:,p:]
-    X = np.zeros((nCh*p, nTr, nT-p))
     
+    X = np.zeros((nCh*p, nTr, nT-p))
     for i in range(p):
         X[nCh*i:nCh*(i+1)] = data[:,:,i:i-p]
-    return X, Y
+        
+    if Inp is None:
+        return X, Y
+    else:
+        U = Inp[:,:,p:]
+        return X, Y, U
 
 def AR_UNSTACK_A(A, p):
     nCh = A.shape[0]
@@ -35,31 +40,42 @@ def AR_UNSTACK_A(A, p):
     
 #     return np.linalg.norm(eps)**2
 
-def AR_PREDICT(data, p, A_LST, U=None, B=None):
+def AR_PREDICT(data, p, A_LST, Inp=None, B=None):
     ABIG = np.hstack(A_LST)
-    XBIG, Y = AR_STACK_LAG(data, p)
     
-    rez = np.einsum('ai,ijk', ABIG, XBIG)
-    if U is not None:
-        rez += np.einsum('ai,ijk', B, U)
-    
+    if Inp is None:
+        XBIG, Y = AR_STACK_LAG(data, p)
+        rez = np.einsum('ai,ijk', ABIG, XBIG)
+    else:
+        XBIG, Y, U = AR_STACK_LAG(data, p, Inp)
+        
+        rez = np.einsum('ai,ijk', ABIG, XBIG)
+        rez += np.einsum('ai,ijk', B, U)    
     return rez
 
-def AR_MLE(data, p, U = None):
-    # Construct stacked past measurements
-    X, Y = AR_STACK_LAG(data, p)
-    
-    # Construct linear system for transition matrices
-    M11 = np.einsum('ajk,bjk', X, Y)
-    M12 = np.einsum('ajk,bjk', X, X)
-    
-    if U is None:
+def AR_MLE(data, p, Inp = None):
+
+    if Inp is None:
+        # Construct stacked past measurements
+        X, Y = AR_STACK_LAG(data, p)
+        
+        # Construct linear system for transition matrices
+        M11 = np.einsum('ajk,bjk', X, Y)
+        M12 = np.einsum('ajk,bjk', X, X)
+        
+        # Solve system
         REZ_A = np.linalg.inv(M12).dot(M11).T
         return AR_UNSTACK_A(REZ_A, p)
-    else:    
+    else:
+        # Construct stacked past measurements
+        X, Y, U = AR_STACK_LAG(data, p, Inp)
+        
+        # Construct linear system for transition matrices
+        M11 = np.einsum('ajk,bjk', X, Y)
+        M12 = np.einsum('ajk,bjk', X, X)
         M13 = np.einsum('ajk,bjk', X, U)
         M21 = np.einsum('ajk,bjk', U, Y)
-        M22 = C.T  #np.einsum('ajk,bjk', U, X)
+        M22 = M13.T  #np.einsum('ajk,bjk', U, X)
         M23 = np.einsum('ajk,bjk', U, U)
 
         # Solve system
