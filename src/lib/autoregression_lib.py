54b4704d3f65b50f8c9d4abbbacd592ca84eaf3f
Aleksejs Fomins <fomins.aleksejs@gmail.com>
2019-05-30 12:18:01 +0200

    handwritten-mar

--- src/lib/autoregression_lib.py
+++ src/lib/autoregression_lib.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 '''
 Algorithm-AR(p)-Multivariate-WithTrials
 
@@ -8,48 +10,70 @@ x(t) = sum_i A(i)x(t-i) + B u(t)
 '''
 
 # Construct stacked past measurements
-def AR_STACK_LAG(Y, p):
-    nCh, nTr, nT = Y.shape()
-    X = np.zeros((nCh*p, nTr, nT))
+def AR_STACK_LAG(data, p):
+    nCh, nTr, nT = data.shape
+    
+    Y = data[:,:,p:]
+    X = np.zeros((nCh*p, nTr, nT-p))
+    
     for i in range(p):
-        X[nCh*i:nCh*(i+1),:,i:] = Y[:,:,i:]
-    return X
+        X[nCh*i:nCh*(i+1)] = data[:,:,i:i-p]
+    return X, Y
 
-# Compute L2 norm of the fit
-def AR_L2(Y, U, p, A, B):
-    ABIG = np.hstack(A)
-    XBIG = AR_STACK_LAG(Y, p)
-    
-    eps = Y - ABIG.dot(XBIG) - B.dot(U)
-    return np.linalg.norm(eps)**2
-    
+def AR_UNSTACK_A(A, p):
+    nCh = A.shape[0]
+    return [A[:, nCh*i:nCh*(i+1)] for i in range(p)]
 
-def AR_MLE(Y, U, p):
-    nCh, nTr, nT = Y.shape()
+# # Compute L2 norm of the fit
+# def AR_L2(data, p, A_LST, U=None, B=None):    
+#     ABIG = np.hstack(A_LST)
+#     XBIG, Y = AR_STACK_LAG(data, p)
     
+#     eps = Y - np.einsum('ai,ijk', ABIG, XBIG)
+#     if U is not None:
+#         eps -= np.einsum('ai,ijk', B, U)
+    
+#     return np.linalg.norm(eps)**2
+
+def AR_PREDICT(data, p, A_LST, U=None, B=None):
+    ABIG = np.hstack(A_LST)
+    XBIG, Y = AR_STACK_LAG(data, p)
+    
+    rez = np.einsum('ai,ijk', ABIG, XBIG)
+    if U is not None:
+        rez += np.einsum('ai,ijk', B, U)
+    
+    return rez
+
+def AR_MLE(data, p, U = None):
     # Construct stacked past measurements
-    X = AR_STACK_LAG(Y, p)
+    X, Y = AR_STACK_LAG(data, p)
     
     # Construct linear system for transition matrices
-    A = np.einsum('ajk,bjk', X, Y)
-    B = np.einsum('ajk,bjk', X, X)
-    C = np.einsum('ajk,bjk', X, U)
-    D = np.einsum('ajk,bjk', U, Y)
-    E = C.T  #np.einsum('ajk,bjk', U, X)
-    F = np.einsum('ajk,bjk', U, U)
+    M11 = np.einsum('ajk,bjk', X, Y)
+    M12 = np.einsum('ajk,bjk', X, X)
     
-    # Solve system
-    BINV = np.linalg.inv(B)
-    FINV = np.linalg.inv(F)
-    TMP11 = A - C.dot(FINV.dot(D))
-    TMP12 = B - C.dot(FINV.dot(E))
-    TMP21 = D - E.dot(BINV.dot(A))
-    TMP22 = F - E.dot(BINV.dot(C))
+    if U is None:
+        REZ_A = np.linalg.inv(M12).dot(M11).T
+        return AR_UNSTACK_A(REZ_A, p)
+    else:    
+        M13 = np.einsum('ajk,bjk', X, U)
+        M21 = np.einsum('ajk,bjk', U, Y)
+        M22 = C.T  #np.einsum('ajk,bjk', U, X)
+        M23 = np.einsum('ajk,bjk', U, U)
 
-    REZ_A = np.linalg.inv(TMP12).dot(TMP11).T
-    REZ_B = np.linalg.inv(TMP22).dot(TMP21).T
-    
-    # Unstack A matrices
-    REZ_A_LST = [REZ_A[nCh*i:nCh*(i+1)] for i in range(p)]
-    
-    return REZ_A_LST, REZ_B
+        # Solve system
+        M12INV = np.linalg.inv(M12)
+        M23INV = np.linalg.inv(M23)
+        TMP11 = M11 - M13.dot(M23INV.dot(M21))
+        TMP12 = M12 - M13.dot(M23INV.dot(M22))
+        TMP21 = M21 - M22.dot(M12INV.dot(M11))
+        TMP22 = M23 - M22.dot(M12INV.dot(M13))
+
+        REZ_A = np.linalg.inv(TMP12).dot(TMP11).T
+        REZ_B = np.linalg.inv(TMP22).dot(TMP21).T
+
+        # Unstack A matrices
+        REZ_A_LST = AR_UNSTACK_A(REZ_A, p)
+
+        return REZ_A_LST, REZ_B
